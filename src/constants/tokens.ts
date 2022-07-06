ea7d56f080d27c3b44fee4d8a8b88b8e5fc54a5a
cyberminter <cyberminter@gmail.com>
2022-07-05 23:09:09 -0600

    Your message

---
+++ src/constants/tokens.ts
@@ -0,0 +1,21 @@
+export const tokens = [
+  {
+    name: 'Wrapped ETC',
+    address: '0x82A618305706B14e7bcf2592D4B9324A366b6dAd',
+    symbol: 'WETC',
+    decimals: 18,
+    chainId: 61,
+    logoURI:
+      'https://raw.githubusercontent.com/cyberminter/cybermint/main/logos/cybermintGreen/etc.png'
+  },
+  {
+    name: 'Bright Coin',
+    address: '0x3d39756b1FF9C68a4E552aE5722cFAF4b8BdB543',
+    symbol: 'Blub',
+    decimals: 3,
+    chainId: 61,
+    logoURI:
+      'https://raw.githubusercontent.com/cyberminter/cybermint/main/logos/cybermintGreen/brightCoin.png'
+  },
+  
+]
